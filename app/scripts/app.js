c8ee37894ec7e704de7bd44133c8af4a64fd4291
Bruno Wu <wubr2000@1-36-58-016.static.netvigator.com>
2013-10-06 19:18:57 +0800

    added bootbox animation, box-shadows

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,6 +35,10 @@ function isTopHorizontalThreeOccupiedByMe() {
 		winning[x] = document.getElementById('cell' + (x+1)).innerHTML;
 	}
 	
-	if (winning[0]!=""&&winning[0]==winning[1]&&winning[1]==winning[2]) { alert("You Won!"); }
+	if (winning[0]!=""&&winning[0]==winning[1]&&winning[1]==winning[2]) { 
+
+		bootbox.alert("You Won!");
+
+	 }
 }
 
